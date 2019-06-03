3bd4a601599a87c455064d7328e76fcda08495b1
Stanislav Kniazev <ssshuler@gmail.com>
2019-06-03 22:33:28 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageToPdfTest {
+   class Program {
+      static void Main(string[] args) {
+         string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+         MagickNET.SetGhostscriptDirectory(path);
+         string fileName = Path.Combine(path, "480x600.jpg");
+         if(!File.Exists(fileName)) {
+            throw new FileNotFoundException("File not found", fileName);
+         }
+         string pdfFileName = Path.Combine(path, "test.pdf");
+         if(File.Exists(pdfFileName)) {
+            File.Delete(pdfFileName);
+         }
+         using(var images = new MagickImageCollection()) {
+            images.Add(new MagickImage(fileName));
+            images.Write(pdfFileName);
+            if(!File.Exists(pdfFileName)) {
+               throw new FileNotFoundException("Pdf creation failed", pdfFileName);
+            }
+         }
+      }
+   }
+}
